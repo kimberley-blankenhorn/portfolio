0940f9c1f565db2dbfc8db2951490a8a40b1e412
Kimberley Blankenhorn <kimberley.blankenhorn@gmail.com>
2022-01-10 15:17:10 +0100

    Added more photos to fill up space

--- components/About/index.js
+++ components/About/index.js
@@ -19,7 +19,7 @@ const About = ({ data }) => {
         style={{
           backgroundImage: `url('${image}')`,
           backgroundPosition: 'bottom',
-          backgroundSize: 'contain',
+          backgroundSize: 'inherit',
         }}
       />
     </div>
